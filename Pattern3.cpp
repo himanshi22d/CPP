bd571ecbdb5acf476637055a6699fe9525cd3342
himanshi <108574343+himanshi22d@users.noreply.github.com>
2024-01-14 00:34:27 +0530

    Pattern3

---
+++ Pattern3.cpp
@@ -0,0 +1,24 @@
+Ques: Print this Pattern
+1
+12
+123
+1234
+12345
+
+Solution:
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n=5;
+    for(int i=1;i<=n;i++)
+    {
+        for(int j=1;j<=i;j++)
+        {
+            cout<<j;
+        }
+        cout<<endl;
+    }
+    return 0;
+}
