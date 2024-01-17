f077582d413a5f161b39cacb1363bd553c781ae3
himanshi <108574343+himanshi22d@users.noreply.github.com>
2024-01-17 23:42:01 +0530

    Pattern6

---
+++ Pattern6.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int countDigits(int n)
+{
+    int count=0;
+    while(n!=0)
+    {
+        count++;
+        n=n/10;
+    }
+    return count;
+}
+
+int main() {
+    int n;
+    cin>>n;
+    cout<<countDigits(n)<<endl;
+    return 0;
+}
