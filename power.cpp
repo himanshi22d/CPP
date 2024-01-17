d0f94da2d4bd1234890e6b74035003a5d2982abf
himanshi <108574343+himanshi22d@users.noreply.github.com>
2024-01-17 22:26:29 +0530

    Calculating power

---
+++ power.cpp
@@ -0,0 +1,38 @@
+Ques: Calculating power using recursion
+
+Sol:
+#include <iostream>
+using namespace std;
+
+int power(int a, int b)
+{
+    if(a==0)
+    {
+        return 0;
+    }
+    if(b==0)
+    {
+        return 1;
+    }
+    if(b==1)
+    {
+        return a;
+    }
+    int ans = power(a,b/2);
+    if(b%2==0)
+    {
+        return ans*ans;
+    }
+    else
+    {
+        return a*ans*ans;
+    }
+}
+
+int main() {
+    int a,b;
+    cin>>a>>b;
+    int ans = power(a,b);
+    cout<<ans;
+    return 0;
+}
