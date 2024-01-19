3d9f8be67a1f266e2b65ac510002614f514cd6f4
himanshi <108574343+himanshi22d@users.noreply.github.com>
2024-01-19 09:44:27 +0530

    Binary Search

---
+++ binarySearch.cpp
@@ -0,0 +1,29 @@
+Ques: Binary Search 
+
+Sol:
+class Solution {
+public:
+    int search(vector<int>& nums, int target) {
+        int s = 0;
+        int e = nums.size()-1;
+        int mid = s + (s+e)/2;
+
+        while(s<=e)
+        {
+            if(nums[mid] == target)
+            {
+                return mid;
+            }
+            if(nums[mid] > target)
+            {
+                e = mid-1;
+            }
+            else
+            {
+                s = mid+1;
+            }
+            int mid = s + (e-s)/2;
+        }
+        return -1;
+    }
+};
