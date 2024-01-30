33bf5cb8d7cb599acccbf2d5b6cdff2f66a1e4b2
himanshi <108574343+himanshi22d@users.noreply.github.com>
2024-01-30 10:39:01 +0530

    Array Partition

---
+++ arrayPartition.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int arrayPairSum(vector<int>& nums) {
+        sort(nums.begin(), nums.end());
+        int n = nums.size();
+        int sum = 0;
+        for(int i=0; i<n; i+=2)
+        {
+            sum+=nums[i];
+        }
+        return sum; 
+    }
+};
