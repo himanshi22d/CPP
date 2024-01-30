b5102f201f8bdc186b28c7ea05be6bbda45038ac
himanshi <108574343+himanshi22d@users.noreply.github.com>
2024-01-30 11:58:17 +0530

    Create power

---
+++ powerOfB.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int power(int a, int b)
+{
+    if(b == 0)
+    {
+        return 1;
+    }
+    return power(a, b-1)*a;
+}
+int main() {
+    cout<<power(2,3);
+    return 0;
+}
