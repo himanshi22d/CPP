e05f6c5940c02152be79103e93d9e8a4a11d2d07
himanshi <108574343+himanshi22d@users.noreply.github.com>
2024-02-15 15:25:43 +0530

    Reverse String with Stack

---
+++ reverseString.cpp
@@ -0,0 +1,26 @@
+Ques: Reverse a String using Stack
+
+Sol:
+#include <iostream>
+#include<stack>
+using namespace std;
+
+int main() {
+    string str = "himanshi";
+    
+    stack<char> s;
+    for(int i=0 ;i<str.length() ;i++ )
+    {
+        char ch = str[i];
+        s.push(ch);
+    }
+    string ans = " ";
+    while(!s.empty())
+    {
+        char ch = s.top();
+        ans.push_back(ch);
+        s.pop();
+    }
+    cout<<"answer is: "<<ans<<endl;
+    return 0;
+}
