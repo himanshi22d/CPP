6f4f2b71785ed065421ef397757ab1acb5606601
himanshi <108574343+himanshi22d@users.noreply.github.com>
2024-02-19 20:17:53 +0530

    Middle Of The Linked List

---
+++ middleOfLinkedList.cpp
@@ -0,0 +1,22 @@
+public class MiddleOfTheLinkedList {
+
+  ListNode middleNode(ListNode head) {
+
+    ListNode slowPtr = head;
+    ListNode fastPtr = head;
+
+    // Travel until the fast pointer reaches
+    // the last node or null
+    while (fastPtr != null && fastPtr.next!= null) {
+
+      // Slow pointer moves 1 node
+      slowPtr = slowPtr.next;
+
+      // Fast pointer moves 2 nodes
+      fastPtr = fastPtr.next.next;
+    }
+
+    return slowPtr;
+  }
+
+}
